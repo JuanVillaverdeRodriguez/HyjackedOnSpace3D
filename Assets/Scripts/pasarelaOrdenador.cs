363f448559cbfb66bf407db244ec2a3b6c2795f5
Carlos Rojo <carlos.rojo@udc.es>
2024-04-22 16:54:27 +0200

    Rebase in progress

---
+++ Assets/Scripts/pasarelaOrdenador.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pasarelaOrdenador : MonoBehaviour, Interactable
+{
+    [SerializeField] private string _prompt;
+
+    public string InteractPrompt => _prompt;
+
+    private bool rotating = false;
+    private float rate = -20.0f;
+    private float count = 0.0f;
+
+    public void Interact(Interactor interactor)
+    {
+        if (count == 0){
+            rotating = true;
+        }
+        Debug.Log("RotandoPasarela");
+    }
+
+    void Update()
+    {
+        if(rotating){
+            if(count > -90.0f){
+                GameObject pasarela = GameObject.Find("PasarelaMovil");
+                if(pasarela != null)
+                {
+                    pasarela.transform.Rotate(Vector3.up*rate*Time.deltaTime);
+                }
+                count += Time.deltaTime * rate;
+            }else{
+                count=0.0f;
+                rotating=false;
+            }
+        }
+    }
+}
